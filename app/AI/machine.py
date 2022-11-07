21c362cc6ea9e2eb6d62cc8f31ff6d5912dbe93f
ORobsonJr <robsonjrservices@gmail.com>
2022-11-07 18:00:50 -0300

    finished

--- app/AI/machine.py
+++ app/AI/machine.py
@@ -1,11 +1,43 @@
 from crud import CRUD 
 
+"""
+Main file in /AI
+"""
+
 class AI():
     def __init__(self) -> None:
         pass
 
     def __main__(self, message, context: list = []):
+        """
+        Call functions from crud file
+        """
+        C = CRUD()
+
+        
         if context:
-            return CRUD().getMessage(message, context)
-            
-        return CRUD().getMessage(message)
+            #If context keyword was provided
+            r = C.getMessage(message, context) #Return message
+
+            if not r: #Any result provided
+                nt = C.messageNtFound(message, context) #Message wasn't received
+                return nt
+
+            return r
+
+        #With no keyword context
+        r = C.getMessage(message) 
+
+        if not r: #Message wasn't received
+            return C.messageNtFound(message)
+
+        return r
+
+    def learnNew(self, message_learn: str, response_: dict):
+        #Learn new words
+        C = CRUD()
+
+        att = C.learnNew(message_to_learn=message_learn, response=response_)
+        return att
+        
+        
