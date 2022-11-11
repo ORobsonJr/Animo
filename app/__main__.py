6670cc363a0dbcc870b231aec3fdbda0466efdca
ORobsonJr <robsonjrservices@gmail.com>
2022-11-11 19:39:19 -0300

    Update, nothing more

--- app/__main__.py
+++ app/__main__.py
@@ -2,15 +2,25 @@ from multiprocessing import Process
 from .BOT.bot import BOT
 from . import server
 from os import system
+from requests.exceptions import ReadTimeout
+from telebot.apihelper import ApiTelegramException
+from sys import exit
 
 def server():
-    print(system('pwd'))
     try: system('cd app/serverAPI/ && uvicorn server:APP --reload') #Run api server
-    except Exception as e: print('[ERRO_SERVER]',e)
+    except Exception as e:
+        print('[ERRO_SERVER]',e)
 
 def bot():
-    BOT().bot() #Run telegram bot
+    try:
+        BOT().bot() #Run telegram bot
 
+    except ApiTelegramException as e:
+        print(e)
+        exit()
+
+    
+        
 if __name__ == '__main__': #Run in paralell
   p1 = Process(target=server)
   p2 = Process(target=bot)
